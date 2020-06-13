298ff9df0922895e41ebf3748bf07ff0e09a767b
Lei Zheng <zhenglei2010fall@gmail.com>
2020-06-13 00:29:20 -0400

    add Linked List Impl/Test

---
+++ src/test/java/amazon/linkedList/MergeTwoSortedTest.java
@@ -0,0 +1,13 @@
+package amazon.linkedList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergeTwoSortedTest {
+
+    @Test
+    public void mergeTwoLists() {
+
+    }
+}
